b32d12c5bb2fb534dc69e45a878e8633586fdab6
cryslei <crysleiferreiragomes1998@gmail.com>
2020-07-04 05:37:37 -0300

    primeiro upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CategoriesModule } from './pages/categories/categories.module';
+
+const routes: Routes = [
+  { path: 'categories', loadChildren: () => CategoriesModule }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
